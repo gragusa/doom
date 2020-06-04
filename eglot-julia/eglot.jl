85513dccef2772a21ee75278024ebc78e852a5ed
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2020-06-04 20:14:14 +0200

    deleted

--- eglot-julia/eglot.jl
+++
@@ -1,8 +0,0 @@
-import Pkg
-Pkg.activate(@__DIR__)
-
-using LanguageServer
-
-server = LanguageServer.LanguageServerInstance(stdin, stdout, false,
-                                               ARGS[1], ARGS[2], Dict())
-run(server)
