3b7cdf883fdc6c0bc24ed1f988fc1cefd30818e8
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2020-06-04 20:15:50 +0200

    Added snippets

---
+++ snippets/org-mode/python-src.py
@@ -0,0 +1,9 @@
+# -*- mode: snippet -*-
+# name: jupyter python src block
+# key: <spy
+# contributor: Giuseppe Ragusa <gragusa@luiss.it>
+# group: org-mode
+# --
+#+BEGIN_SRC python :session myp :async yes :export both :results output
+$0
+#+END_SRC
